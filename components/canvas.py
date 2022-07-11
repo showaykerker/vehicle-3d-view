9c7fe266c068a44d28eb3c3a95e16feee558c3a3
Hsu Hsiu Wei <zackhsu@itri.org.tw>
2022-07-12 00:33:18 +0800

    Add wait_time to canvas.py

--- components/canvas.py
+++ components/canvas.py
@@ -4,7 +4,10 @@ import cv2
 class Canvas:
 	def __init__(self, size=(400, 600)):
 		self._size = np.array([size[0], size[1], 3])
-		self._canvas = np.ones(self._size, dtype="uint8") * 192
+		self.clear()
+
+	def clear(self):
+		self._canvas = np.ones(self._size, dtype="int32") * 192
 
 	def add_line(self, start, end, color, width=2):
 		cv2.line(
@@ -14,7 +17,11 @@ class Canvas:
 			color.tolist(),
 			width)
 
-	def show(self):
-		cv2.imshow("view", self._canvas)
-		cv2.waitKey(0)
+	def show(self, wait_time=0):
+		cv2.imshow("view", self._canvas.astype(np.uint8))
+		cv2.waitKey(wait_time)
+		if wait_time == 0:
+			self.kill()
+
+	def kill(self):
 		cv2.destroyAllWindows()
